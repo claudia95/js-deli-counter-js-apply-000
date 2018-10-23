78632900bc09f3bc0ae32195076c1cc4fc3f03de
Claudia Fox <claudiafox95@icloud.com>
2018-10-23 12:25:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."; 
   } else {
-    for (i = 0; i > katzDeliLine.length; i++) {
+    for (var i = 0; i > katzDeliLine.length; i++) {
       current.push(i+1 + ". " + katzDeliLine[i]);
       return "The line is currently: " + current.join(', ');
     } 
